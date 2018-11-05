f809aa091f07afec45ce82e59446d46469133b19
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-11-05 12:15:52 -0700

    usage of es6 modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { groupBy } from "lodash-es"
+import people from './people'
+
+const managerGroups = groupBy(people, "manager");
+
 const root = document.createElement('div');
-root.innerHTML = `<p>Hello Webpack</p>`;
-document.body.appendChild(root);
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
+document.body.appendChild(root)
+
---
+++ src/people.js
@@ -0,0 +1,16 @@
+const people = [
+  {
+    manager: "Jen",
+    name: "Bob"
+  },
+  {
+    manager: "John",
+    name: "Sue"
+  },
+  {
+    manager: "Stan",
+    name: "Shirley"
+  }
+]
+
+export default people
